f30b972d70322c56b273de206ca70d8fd3bf186d
Gioele Bucci <gioelebucci@gmail.com>
2024-06-15 17:57:21 +0200

    edit DB name in connection string

--- Assets/Scripts/utils/SqlUtils.cs
+++ Assets/Scripts/utils/SqlUtils.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public static class SqlUtils
 {
-  const string connectionString = "server=localhost ; database=unitytest; user=root ; password= ; charset=utf8 ; SslMode=None;";
+  const string connectionString = "server=localhost ; database=RisiKoDB; user=root ; password= ; charset=utf8 ; SslMode=None;";
   public static MySqlConnection NewConnection()
   {
     return new MySqlConnection(connectionString);
